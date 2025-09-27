3c90ff210d6895fcdcca8808359604e955f68e81
Martin <martin.valenzuela2501@alumnos.ubiobio.cl>
2025-09-27 17:58:20 -0300

    Clase Cuadrilla Terminada

--- src/Cuadrilla.java
+++ src/Cuadrilla.java
@@ -1,15 +1,27 @@
+import java.time.LocalDate;
 import java.util.Date;
 
 public class Cuadrilla {
     private int id;
     private String nombre;
     private static int maximoCosechadores;
+    private Supervisor supervisor;
+    private PlanCosecha planCosecha;
+    private CosechadorAsignado[] asignaciones;
+    private int nroAsignaciones;
 
     public Cuadrilla(int id, String nom, Supervisor sup, PlanCosecha plan) {
         this.id = id;
         this.nombre = nom;
         this.supervisor = sup;
         this.planCosecha = plan;
+        this.maximoCosechadores = 0;
+        this.asignaciones = new CosechadorAsignado[50];
+        this.nroAsignaciones = 0;
+
+        if(sup != null) {
+            sup.setCuadrilla(this);
+        }
     }
 
     public int getId() {
@@ -29,17 +41,39 @@ public class Cuadrilla {
     public PlanCosecha getPlanCosecha(){
         return planCosecha;
     }
-    public boolean addCosechador (Date fIni, Date fFin, double meta, Cosechador cos){
 
+    private Cosechador findCosechadorByRut(Cosechador cos) {
+        for(int i = 0; i < nroAsignaciones; i++) {
+            if(asignaciones[i].getCosechador().getRut().equals(cos.getRut())) {
+                return asignaciones[i].getCosechador();
+            }
+        }
+        return null;
+    }
+    public boolean addCosechador (LocalDate fIni, LocalDate fFin, double meta, Cosechador cosechador){
+        if(nroAsignaciones >= maximoCosechadores){
+            return false;
+        }
+        if(findCosechadorByRut(cosechador) != null){
+            return false;
+        }
+
+        CosechadorAsignado nuevo = new CosechadorAsignado(fIni, fFin, meta, this, cosechador);
+        asignaciones[nroAsignaciones++] = nuevo;
+        return true;
     }
     public Cosechador[] getCosechadores(){
-
+        Cosechador[] resultado = new Cosechador[nroAsignaciones];
+        for (int i = 0; i < nroAsignaciones; i++) {
+            resultado[i] = asignaciones[i].getCosechador();
+        }
+        return resultado;
     }
     public static int getMaximoCosechadores(){
-
+        return  maximoCosechadores;
     }
 
     public static void setMaximoCosechadores(int max) {
-        this.maximoCosechadores = max;
+        maximoCosechadores = max;
     }
 }
