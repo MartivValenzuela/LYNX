d1afaac29f85dbeb8fa9f760d1089766c82fdf0a
AlexVeez32 <alex.vera2401@alumnos.ubiobio.cl>
2025-09-29 18:09:41 -0300

    hice modificaciones en la clase propietario creo que por ahora esta bien

--- src/Propietario.java
+++ src/Propietario.java
@@ -1,19 +1,37 @@
-public class Propietario {
-    private String direccionComercial;
+import java.util.ArrayList;
 
-    public String getDireccionComercial() {
+public class Propietario extends Persona {
+    private String direccionComercial;
+    private ArrayList<Huerto> huertos;
+
+    public Propietario(Rut rut, String nombre, String email, String direccion, String direccionComercial) {
+        super(rut, nombre, email, direccion);
+        this.direccionComercial = direccionComercial;
+        this.huertos = new ArrayList<>();
+    }
+
+    public String getDirComercial() {
         return direccionComercial;
     }
 
-    public void setDireccionComercial(String direccionComercial) {
-        this.direccionComercial = direccionComercial;
+    public void setDirComercial(String direccion) {
+        this.direccionComercial = direccion;
     }
 
-    public boolean addHuerto(Huerto huerto){
-
+    public boolean addHuerto(Huerto huerto) {
+        // hay relacion con Huerto
+        if (!huertos.contains(huerto)) {
+            huertos.add(huerto);
+            return true;
+        }
+        return false;
     }
-    public Huerto[] getHuertos(){
-        return huertos;
-    }
 
+    public Huerto[] getHuertos() {
+        return huertos.toArray(new Huerto[0]);
+    }
+    // meteodo agregado todavia sin uso podriia llegar a servir (opcional)
+    public int getCantidadHuertos() {
+        return huertos.size();
+    }
 }
