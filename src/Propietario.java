74f8ae34f3347caee83fc09bb132a2a0edb89aca
AlexVeez32 <alex.vera2401@alumnos.ubiobio.cl>
2025-09-30 16:30:06 -0300

    probando...

--- src/Propietario.java
+++ src/Propietario.java
@@ -1,37 +1,20 @@
-import java.util.ArrayList;
-
 public class Propietario extends Persona {
     private String direccionComercial;
-    private ArrayList<Huerto> huertos;
-
     public Propietario(Rut rut, String nombre, String email, String direccion, String direccionComercial) {
         super(rut, nombre, email, direccion);
         this.direccionComercial = direccionComercial;
-        this.huertos = new ArrayList<>();
     }
-
     public String getDirComercial() {
         return direccionComercial;
     }
-
     public void setDirComercial(String direccion) {
         this.direccionComercial = direccion;
     }
-
     public boolean addHuerto(Huerto huerto) {
-        // hay relacion con Huerto
-        if (!huertos.contains(huerto)) {
-            huertos.add(huerto);
-            return true;
-        }
-        return false;
-    }
 
-    public Huerto[] getHuertos() {
-        return huertos.toArray(new Huerto[0]);
+        return true;
     }
-    // meteodo agregado todavia sin uso podriia llegar a servir (opcional)
-    public int getCantidadHuertos() {
-        return huertos.size();
+    public Huerto[] getHuertos() {
+        return new Huerto[0];
     }
 }
--- src/Supervisor.java
+++ src/Supervisor.java
@@ -1,11 +1,9 @@
 public class Supervisor extends Persona {
     private String profesion;
-    private Cuadrilla cuadrilla;
 
     public Supervisor(Rut rut, String nombre, String email, String direccion, String profesion) {
         super(rut, nombre, email, direccion);
         this.profesion = profesion;
-        this.cuadrilla = null;
     }
 
     public String getProfesion() {
@@ -16,18 +14,9 @@ public class Supervisor extends Persona {
         this.profesion = profesion;
     }
 
-    public Cuadrilla getCuadrilla() {
-        return cuadrilla;
-    }
-
     public void setCuadrilla(Cuadrilla cuad) {
-        // Supervisor controla Cuadrilla
-        this.cuadrilla = cuad;
-        if (cuad != null) {
-
-        }
     }
-    public boolean tieneCuadrillaAsignada() {
-        return cuadrilla != null;
+    public Cuadrilla getCuadrilla() {
+        return null;
     }
 }
