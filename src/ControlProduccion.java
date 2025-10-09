86b20e81166d48d5a676b93a820d21d37bbd12cb
AlexVeez32 <alex.vera2401@alumnos.ubiobio.cl>
2025-10-08 22:08:23 -0300

    metodos agregados

--- src/ControlProduccion.java
+++ src/ControlProduccion.java
@@ -1,14 +1,39 @@
 import java.util.Date;
-
+import java.util.ArrayList;
+import java.util.List;
 public class ControlProduccion {
+    private List<Propietario> propietarios;
+    private List<Supervisor> supervisores;
+    private List<Cosechador> cosechadores;
+
+    public ControlProduccion() {
+        this.propietarios = new ArrayList<>();
+        this.supervisores = new ArrayList<>();
+        this.cosechadores = new ArrayList<>();
+        this.generateTestData();
+    }
     public boolean createPropietario (Rut rut, String nombre, String email, String dirParticular, String dirComercial){
-        return ;
+        return;
     }
-    public boolean createSupervisor (Rut rut, String nombre, String email, String dirrecion, String profesion){
-
+    public boolean createSupervisor (Rut rut, String nombre, String email, String direccion, String profesion){
+        for (Supervisor supervisor : supervisores) {
+            if (supervisor.getRut().equals(rut)) {
+                return false;
+            }
+        }
+        Supervisor nuevoSupervisor = new Supervisor(rut, nombre, email, direccion, profesion);
+        supervisores.add(nuevoSupervisor);
+        return true;
     }
-    public boolean createCosechador (Rut rut, String nombre, String email, String dirrecion, Date fechaNacimiento){
-
+    public boolean createCosechador (Rut rut, String nombre, String email, String direccion, Date fechaNacimiento){
+        for (Cosechador cosechador : cosechadores) {
+            if (cosechador.getRut().equals(rut)) {
+                return false;
+            }
+        }
+        Cosechador nuevoCosechador = new Cosechador(rut, nombre, email, direccion, fechaNacimiento);
+        cosechadores.add(nuevoCosechador);
+        return true;
     }
     public boolean createCultivo (int id,String especie, String variedad,float Rendimiento){
 
@@ -29,13 +54,19 @@ public class ControlProduccion {
 
     }
     public String[] listCultivos(){
-
+    return new String[0];
     }
     public String[] listHuertos(){
 
     }
     public String[] listPropietarios(){
-
+        String[] lista = new String[propietarios.size()];
+        for (int i = 0; i < propietarios.size(); i++) {
+            Propietario prop = propietarios.get(i);
+            lista[i] = prop.getRut() + ", " + prop.getNombre() + ", " + prop.getEmail() + ", " +
+                    prop.getDireccion() + ", " + prop.getDirComercial();
+        }
+        return lista;
     }
     public String[] listSupervisores(){
 
@@ -47,6 +78,13 @@ public class ControlProduccion {
 
     }
     private void generateTestData(){
+            propietarios.add(new Propietario(new Rut("11.111.111-1"), "Daniel Ruiz Saez",
+                    "daniel.ruiz@email.com", "Los Alerces 123", "Calle Comercial 456"));
+            supervisores.add(new Supervisor(new Rut("22.222.222-2"), "Leonora Casas Solís",
+                    "leonora.casas@gmail.com", "Los pinguinos 432", "Agronomo"));
+            cosechadores.add(new Cosechador(new Rut("33.333.333-3"), "David Rios Flores",
+                    "david.riosf@gmail.com", "Las Amapolas 234", new Date()));
+        }
 
     }
 }
