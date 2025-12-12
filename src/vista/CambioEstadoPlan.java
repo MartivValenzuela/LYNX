50e04e39e21085840e6cd63e1827e2d9f1e71fce
topph <harold.topp2401@alumnos.ubiobio.cl>
2025-12-12 03:05:55 -0300

    .

--- src/vista/CambioEstadoPlan.java
+++ src/vista/CambioEstadoPlan.java
@@ -11,7 +11,7 @@ public class CambioEstadoPlan extends JDialog {
     private JButton buttonOK;
     private JButton buttonCancel;
     private JComboBox<String> comboBox1;
-    private JTextField idplangui;
+    private JTextField idplangui ;
     private JLabel nombregui;
 
 
@@ -49,26 +49,34 @@ public class CambioEstadoPlan extends JDialog {
         }, KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0), JComponent.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT);
 
 
+
+        idplangui.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyTyped(KeyEvent e) {
+                char c = e.getKeyChar();
+                if (!Character.isDigit(c) && !Character.isSpaceChar(c)){
+                    e.consume();
+                }
+
+            }
+        });
     }
 
-    private void mostrarPlanCosecha() {
-        // PlanCosecha plan = new PlanCosecha();
-         // nombregui = plan
 
-    }
 
 
     private void onOK() {
 
-        Integer idplangui = null;
+
         try {
-            String idplan = this.idplangui.getText().trim();
+            String idplan = idplangui.getText();
 
             if (idplan.isEmpty()){
                 JOptionPane.showMessageDialog(this, "El campo ID Plan está vacío", "Error", JOptionPane.ERROR_MESSAGE);
                 return;
             }
-            idplangui = Integer.valueOf(idplan);
+
+
         }catch (NumberFormatException e){
             JOptionPane.showMessageDialog(this, "ID inválido", "Error", JOptionPane.ERROR_MESSAGE);
             return;
